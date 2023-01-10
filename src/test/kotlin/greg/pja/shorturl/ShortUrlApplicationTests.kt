1ffa3ba5041ad8e875ca8cbeca982e43b24fdd55
mgregorini <marco.gregorini@project-a.com>
2023-01-10 11:31:45 +0100

    firt commit

---
+++ src/test/kotlin/greg/pja/shorturl/ShortUrlApplicationTests.kt
@@ -0,0 +1,13 @@
+package greg.pja.shorturl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
