04b5a0c90dc97a9b7fe507c03b297f9cb32d5703
Marco Gregorini <46004682+GregPja@users.noreply.github.com>
2023-01-10 12:16:43 +0100

    Added base setup for jooq, flyway and docker-compose

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # SHORT-URL
 
+## How to run the project
+
+
+To prepare the service simply run this command:
+* `make init`
+  * to set up the db, migration and generating jooq's classes
+
+
+### Other commands
+
+* Run the DB with docker-compose:
+  * `make db-up`
+* Apply migrations to DB
+  * `make migrate`
+* Generate jooq's classes
+  * `make migrate`
+
+
+### Troubleshooting
+
+This project requires Java17 to run, in case the `make init` (or `make migrate`) command is not working for you and you see an error like this:
+
+    Incompatible because this component declares a component compatible with Java 17 and the consumer needed a component compatible with Java 11
+
+It means your terminal's java version is 11 (or lower). You can temporarily fix your Java version by typing:
+
+```bash
+export JAVA_HOME="PATH TO YOUR JAVA 17"
+export PATH=$PATH:$JAVA_HOME
+```
+
+Otherwise, use your IDE's functionality to run gradle's tasks
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jooq.meta.jaxb.Logging
+import org.jooq.meta.jaxb.Property
 
 plugins {
     id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
+    id("org.flywaydb.flyway") version "9.2.0"
+    id("nu.studer.jooq") version "8.0"
 }
 
 group = "greg.pja"
@@ -17,9 +21,15 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.flywaydb:flyway-core:9.2.0")
+    implementation("org.postgresql:postgresql:42.5.1")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    jooqGenerator("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -32,4 +42,56 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/short-url"
+    driver = "org.postgresql.Driver"
+    user = "postgres"
+    password = "example"
+    baselineOnMigrate = true
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
+}
+
+jooq {
+    version.set("3.17.5")
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/short-url"
+                    user = "postgres"
+                    password = "example"
+                    properties = listOf(
+                        Property().apply {
+                            key = "ssl"
+                            value = "false"
+                        }
+                    )
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        excludes = "flyway_schema_history"
+                        inputSchema = "public"
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = false
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "greg.pja.jooq.generated"
+                        directory = "build/generated-src/jooq/main"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
 }
